4a9904a1909bc60d58e0ee95054c961ffe2d17cc
Hongtao Wang <121870469+HungtaoWang@users.noreply.github.com>
2024-10-14 00:37:21 +0800

    Add files via upload

---
+++ script/poetry/train_SAFT_GAU_poetry.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=0 python SAFT/main.py \
+    --large True \
+    --data_path "edge_data/poetry" \
+    --model_type "SAFT_GAU_large" \
+    --epochs 100 \
+    --early_stop 3 \
+    --lr 1e-5 \
+    --weight_decay 1e-3 \
+    --adam_epsilon 1e-8 \
+    --rd 107 \
+    --prop_layers 2 \
+    --tlambda 0.5 \
+    --delta 1.5 \
+    --pe_dim 64 \
+    --train_batch_size 25\
+    --approximate True\
+    --model_name_or_path "bert-base-uncased"
---
+++ script/products/train_SAFT_GAU_products.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=0 python SAFT/main.py \
+    --large True \
+    --data_path "edge_data/products" \
+    --model_type "SAFT_GAU_large" \
+    --epochs 100 \
+    --early_stop 3 \
+    --lr 1e-5 \
+    --weight_decay 1e-3 \
+    --adam_epsilon 1e-8 \
+    --rd 0 \
+    --prop_layers 1 \
+    --tlambda 0 \
+    --delta 0 \
+    --pe_dim 64 \
+    --train_batch_size 25\
+    --approximate True\
+    --model_name_or_path "bert-base-uncased"
