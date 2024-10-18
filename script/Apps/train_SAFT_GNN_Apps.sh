1e8e6fa3474a75a514766419ae1d07fa606cf03e
Hongtao Wang <121870469+HungtaoWang@users.noreply.github.com>
2024-10-18 11:07:04 +0800

    Delete script directory

--- script/Apps/train_SAFT_GNN_Apps.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python SAFT/main.py --large False --data_path "edge_data/Apps" --model_type "SAFT_GNN_small" --epochs 300 --early_stop 30 --lr 1e-3 --weight_decay 1e-2 --adam_epsilon 1e-6 --rd 107 --prop_layers 1 --tlambda 2 --delta 2 --pe_dim 64 --model_name_or_path "prajjwal1/bert-tiny"
--- script/children/train_SAFT_GAU_children.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python SAFT/main.py --large False --data_path "edge_data/children" --model_type "SAFT_GAU_small" --epochs 300 --early_stop 30 --lr 1e-3 --weight_decay 1e-2 --adam_epsilon 1e-6 --rd 0 --prop_layers 2 --tlambda 0.5 --delta 0 --pe_dim 64 --model_name_or_path "prajjwal1/bert-tiny"
